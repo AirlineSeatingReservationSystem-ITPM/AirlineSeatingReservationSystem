b45193263114eefb56058b561e7abdf4276edba1
Kavi-Kariyawasam <it20081584@my.sliit.lk>
2022-04-20 23:53:27 +0530

    edit app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -60,7 +60,7 @@ function App() {
          <Route path="/edit-userM" element={<UserList />} /> 
         
         </Routes>
-        <Footer/>
+        {/* <Footer/> */}
       </Router> 
 
       <Router>
